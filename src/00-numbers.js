d6451ef781346c6e78766f7d9f725a772bb0cc5f
Tom LaTulipe <tlatulipe@gmail.com>
2022-02-05 16:15:43 -0500

    First 2 down

--- src/00-numbers.js
+++ src/00-numbers.js
@@ -4,7 +4,13 @@
  * @param {number[]} values - An array of whole numbers.
  * @returns {number} - The sum of all numbers in the array.
  */
-function addAllNumbers(values) {}
+function addAllNumbers(values) {
+  let sum = 0;
+  for (let number of values) {
+    sum += number;
+  }
+  return sum;
+}
 
 /**
  * Multiply each number in the array by 2 and return a new array with the doubled values.
@@ -12,7 +18,16 @@ function addAllNumbers(values) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {number[]} - An array of whole numbers.
  */
-function doubleAllNumbers(values) {}
+function doubleAllNumbers(values) {
+  let product = 0;
+  let prodArr = [];
+
+  for (let number of values) {
+    prodArr.push(number * 2);
+  }
+
+  return prodArr;
+}
 
 /**
  * Return an array of only the even numbers in the inputted array.
@@ -20,7 +35,17 @@ function doubleAllNumbers(values) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {number[]} - An array of only even numbers.
  */
-function getAllEvenNumbers(values) {}
+function getAllEvenNumbers(values) {
+  let evenArr = [];
+
+  for (number of values) {
+    if (number % 2 == 0) {
+      evenArr.push(number);
+    }
+  }
+
+  return evenArr;
+}
 
 /**
  * Return the number given if it is in the array. Otherwise, return `null`.
@@ -29,7 +54,13 @@ function getAllEvenNumbers(values) {}
  * @param {number} val - A specific value to find.
  * @returns {number|null} - The found number or `null`.
  */
-function findNumberByValue(values, val) {}
+function findNumberByValue(values, val) {
+  if (values.includes(val)) {
+    return val;
+  } else {
+    return null;
+  }
+}
 
 /**
  * Return `true` if all numbers in the array are negative. Otherwise, return `false`. Ignore `0` in determining whether or not an array includes all negative numbers.
@@ -37,7 +68,20 @@ function findNumberByValue(values, val) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {boolean} - Whether or not all numbers in the array are negative.
  */
-function allNumbersAreNegative(values) {}
+function allNumbersAreNegative(values) {
+  let found = 0;
+
+  if (values.length == 0){
+    return false;
+  }
+
+  for (number of values) {
+    if (number > 0) {
+      return false;
+    } 
+  }
+  return true;
+}
 
 /**
  * Return the largest number in the array. If the array is empty, return `null`.
@@ -45,7 +89,19 @@ function allNumbersAreNegative(values) {}
  * @param {number[]} values - An array of whole numbers.
  * @returns {number} - The largest number in the array.
  */
-function getLargestNumber(values) {}
+function getLargestNumber(values) {
+  if (values.length == 0){
+    return null;
+  }
+
+  let largest = values[0];
+  for (let i = 1; i < values.length;i++){
+    if (values[i] > largest){
+      largest = values[i];
+    }
+  }
+  return largest;
+}
 
 module.exports = {
   addAllNumbers,
--- src/01-strings.js
+++ src/01-strings.js
@@ -4,15 +4,29 @@
  * @param {string[]} words - An array of strings.
  * @returns {string} - A single string with all words combined together.
  */
-function combineAllWords(words) {}
+function combineAllWords(words) {
+  let stringAlong = "";
+
+  for (let word of words) {
+    stringAlong += word;
+  }
+  return stringAlong;
+}
 
 /**
  * Return an array of the words but each word is repeated.
  *
  * @param {string[]} words - An array of strings.
- * @returns {string[]} - An array of whole numbers.
+ * @returns {string[]} - An array of whole numbers. --what?
  */
-function repeatAllWords(words) {}
+function repeatAllWords(words) {
+  let strArray = [];
+  for (let word in words) {
+    strArray.push(words[word]);
+    strArray[word] += words[word];
+  }
+  return strArray;
+}
 
 /**
  * Return an array of only those words that have four characters or less.
@@ -20,7 +34,15 @@ function repeatAllWords(words) {}
  * @param {string[]} words - An array of strings.
  * @returns {string[]} - An array of only small words.
  */
-function getSmallWords(words) {}
+function getSmallWords(words) {
+  let returnArr = [];
+  for (let word in words) {
+    if (words[word].length <= 4) {
+      returnArr.push(words[word]);
+    }
+  }
+  return returnArr;
+}
 
 /**
  * Return the first word that includes the character. If no word is found, return `null`.
@@ -29,7 +51,14 @@ function getSmallWords(words) {}
  * @param {string} character - A single character.
  * @returns {string|null} - The found word or `null`.
  */
-function findWordWithCharacter(words, character) {}
+function findWordWithCharacter(words, character) {
+  for (let word of words) {
+    if (word.includes(character)) {
+      return word;
+    }
+  }
+  return null;
+}
 
 /**
  * Return `true` if all words are in all caps. Otherwise, return `false`.
@@ -37,7 +66,15 @@ function findWordWithCharacter(words, character) {}
  * @param {string[]} words - An array of strings.
  * @returns {boolean} - Whether or not all words are in all caps.
  */
-function allWordsAreAllCaps(words) {}
+function allWordsAreAllCaps(words) {
+  for (let word of words) {
+    if (/[a-z]/.test(word)) {
+      // Reference: https://www.w3schools.com/jsref/jsref_regexp_test.asp
+      return false;
+    }
+  }
+  return true;
+}
 
 /**
  * Return the first longest word.
@@ -45,7 +82,17 @@ function allWordsAreAllCaps(words) {}
  * @param {string[]} words - An array of strings.
  * @returns {string} - The first word that is the longest.
  */
-function getLongestWord(words) {}
+function getLongestWord(words) {
+  let longestWord = words[0];
+  longestWord.length;
+  for (let word in words){
+    if (words[word].length > longestWord.length){
+      longestWord = words[word];
+    }
+  }
+  return longestWord;
+
+}
 
 module.exports = {
   combineAllWords,
